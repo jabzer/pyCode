6080e7c16cfcdfc4fa7a69889dc3d857586583e6
jabzer <maihongjieer@qq.com>
2018-03-21 01:01:02 +0800

    开始学习tornado

---
+++ TornadoPro/learn/01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import tornado.web
+import tornado.ioloop
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.write('hello')
+
+
+if __name__ == '__main__':
+    app = tornado.web.Application(
+        [(r'/',IndexHandler)]
+    )
+    app.listen(8000)
+    tornado.ioloop.IOLoop.current().start()
