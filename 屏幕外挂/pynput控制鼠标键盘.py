1475c5e673b30412d180b86cd6b20a4de983e824
jabzer <maihongjieer@qq.com>
2018-03-20 14:37:39 +0800

    控制鼠标

--- 屏幕外挂/pynput控制鼠标键盘.py
+++ 屏幕外挂/pynput控制鼠标键盘.py
@@ -2,6 +2,15 @@
 from pynput.mouse import Button,Controller
 mouse = Controller()
 #定位
-mouse.position = (10,20)
-#移动
-mouse.move(5,-5)
+#mouse.position = (10,20)
+#移动mac
+#mouse.move(5,-5)
+#一直按下
+#mouse.press(Button.left)
+#松开
+#mouse.release(Button.left)
+#双击 mac
+#mouse.position = (20,20)
+#mouse.click(Button.left,2)
+#中轴滚动
+#mouse.scroll(0,2)
