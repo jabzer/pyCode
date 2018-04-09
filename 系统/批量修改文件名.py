8f1950f260b03ed0b06fa37046f8706f1192f686
jabzer <maihongjieer@qq.com>
2018-04-09 16:21:21 +0800

    批量修改文件后缀

---
+++ 系统/批量修改文件名.py
@@ -0,0 +1,22 @@
+import os
+
+path = "D:\\恐怖熱線"
+
+def mp4Tomp3(path):
+    for root,dirs,files in os.walk(path,topdown=True):
+        for i in files:
+            oldname = root+'\\'+i
+            if os.path.splitext(root+'\\'+i)[1] == '.mp4':
+                newname = os.path.splitext(root+'\\'+i)[0]+'.mp3'
+                os.rename(oldname,newname)
+                print(oldname + "--------》"+newname)
+
+def mp3Tomp4(path):
+    for root,dirs,files in os.walk(path,topdown=True):
+        for i in files:
+            oldname = root+'\\'+i
+            if os.path.splitext(root+'\\'+i)[1] == '.mp3':
+                newname = os.path.splitext(root+'\\'+i)[0]+'.mp4'
+                os.rename(oldname,newname)
+
+mp4Tomp3(path)
