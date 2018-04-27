13ec6c061212fbb51586fc6b0d0989ed0131af12
jabzer <maihongjieer@qq.com>
2018-04-27 11:37:33 +0800

    合拼分段视频

--- 系统/使用 Python 把多个 MP4 合成一个视频/合并视频.py
+++ 系统/使用 Python 把多个 MP4 合成一个视频/合并视频.py
@@ -6,20 +6,31 @@ type = '.flv'
 
 
 def Combining(baseDir):
+    print("开始合并:{0}".format(baseDir))
     L = []
     for root, dirs, files in os.walk(baseDir):
         files = natsorted(files)
         for file in files:
             if os.path.splitext(file)[1] == type:
                 filePath = os.path.join(root, file)
-                print(filePath)
                 video = VideoFileClip(filePath)
-                print(video)
+                print("加载文件："+filePath)
                 L.append(video)
-    final_clip = concatenate_videoclips(L)
-    final_clip.write_videofile(baseDir+"\\target.mp4", fps=24, remove_temp=False)
+    if len(L) > 0:
+        final_clip = concatenate_videoclips(L)
+        print("加载完成！\n 开始合并视频...\n")
+        final_name = "_".join(baseDir.split('\\')[-2:])
+        final_clip.write_videofile(baseDir + "\\{0}.mp4".format(final_name), fps=24, remove_temp=True)
+        print("完成：合并目录{0}视频")
+    else:
+        print("此目录没有{0}文件。".format(type))
 
 
 if __name__ == "__main__":
-    baseDir = "D:\\迅雷下载\\10376670\\1"
-    Combining(baseDir)
+    path = "E:\\教程\\哔哩哔哩"
+    for root, dirs, files in os.walk(path):
+        if not dirs:
+            print('root : {0}'.format(root))
+            Combining(root)
+    # baseDir = "D:\\迅雷下载\\10376670\\1"
+    # Combining(baseDir)
