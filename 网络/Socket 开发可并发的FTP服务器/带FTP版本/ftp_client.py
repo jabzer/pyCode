989edb9236e250ad48392cb2c5d7d7edac1400f8
jabzer <maihongjieer@qq.com>
2018-05-08 09:09:41 +0800

    改格式

--- 网络/Socket 开发可并发的FTP服务器/带FTP版本/ftp_client.py
+++ 网络/Socket 开发可并发的FTP服务器/带FTP版本/ftp_client.py
@@ -2,46 +2,46 @@ import os
 import socket
 import json
 
-ip_port=('127.0.0.1',1000)
+ip_port = ('127.0.0.1', 1000)
 s = socket.socket()
 
 s.connect(ip_port)
 
 welcome_msg = s.recv(1024)
 
-print("from server : ",welcome_msg.decode())
+print("from server : ", welcome_msg.decode())
 
 while True:
     send_data = input('put 文件路径 >>').strip()
-    if len(send_data)==0:continue
-    cmd_list =send_data.split()
-    if len(cmd_list)<2:continue
+    if len(send_data) == 0: continue
+    cmd_list = send_data.split()
+    if len(cmd_list) < 2: continue
     task_type = cmd_list[0]
-    if task_type =='put':
+    if task_type == 'put':
         abs_filepath = cmd_list[1]
         if os.path.isfile(abs_filepath):
-            file_size = os.stat(abs_filepath).st_size#获取文件大小
+            file_size = os.stat(abs_filepath).st_size  # 获取文件大小
             file_name = abs_filepath.split('\\')[-1]
-            print('file:{0} size:{1}'.format(abs_filepath,file_size))
-            msg_data={'action':'put','file_name':file_name,'file_size':file_size}
-            s.send(bytes(json.dumps(msg_data),encoding='utf-8'))
+            print('file:{0} size:{1}'.format(abs_filepath, file_size))
+            msg_data = {'action': 'put', 'file_name': file_name, 'file_size': file_size}
+            s.send(bytes(json.dumps(msg_data), encoding='utf-8'))
 
             sever_confim = s.recv(1024)
             confirm_data = json.loads(sever_confim.decode())
 
-            if confirm_data['status']==200:
-                print('开始传输文件：',file_name)
-                f = open(abs_filepath,'rb')
+            if confirm_data['status'] == 200:
+                print('开始传输文件：', file_name)
+                f = open(abs_filepath, 'rb')
                 for line in f:
                     s.send(line)
                 print('发送成功')
                 continue
         else:
-            print("不存在此文件:",abs_filepath)
+            print("不存在此文件:", abs_filepath)
             continue
     else:
         print('不支持此操作')
         continue
     recv_data = s.recv(1024)
-    print(str(recv_data,encoding='utf-8'))
+    print(str(recv_data, encoding='utf-8'))
 s.close()
