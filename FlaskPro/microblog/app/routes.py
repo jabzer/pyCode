085a1742ec224e4061a7ea25f55bae0a5a778bf9
jabzer <maihongjieer@qq.com>
2018-05-29 09:52:14 +0800

    模板

--- FlaskPro/microblog/app/routes.py
+++ FlaskPro/microblog/app/routes.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello"
+    user = {'username':'不知道是什么'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html',title='home',posts = posts,user=user)
